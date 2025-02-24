72705098f4f54c76e3f590226f5d8bf3384d3154
dhanushgowdahm <dhanushhm1012@gmail.com>
2025-02-24 18:00:47 +0530

    list api functions added

--- handler.js
+++ handler.js
@@ -6,10 +6,60 @@ const common = require('./bk-utils/common');
 const rdsOccasions = require('./bk-utils/rds/rds.occasions.helper');
 const rdsOUsers = require('./bk-utils/rds/rds.occasion.users.helper');
 const snsHelper = require('./bk-utils/sns.helper');
+const rdsUsers = require('./bk-utils/rds/rds.users.helper');
 // const rdsPosts = require('./bk-utils/rds/rds.posts.helper');
 
 const { OCCASION_CONFIG } = constants;
 
+async function getOccasions(request) {
+  const { decoded } = request;
+  const { type } = request.queryStringParameters;
+  logger.info({ type });
+  const mJoins = await rdsOUsers.getOccasions(decoded.id);
+  const mIds = mJoins.items.map((i) => i.occasionId);
+  logger.info('occasion ids ', mIds);
+  const vIds = mJoins.items.filter((i) => i.status === OCCASION_CONFIG.status.verified).map((i) => i.occasionId);
+  logger.info('verified occasion ids ', vIds);
+  if (type === 'occasions') {
+    const gbIds = []; // groom & bride Ids
+    const [occasions, bgcounts] = await Promise.all([rdsOccasions.getOccasionsIn(mIds), rdsOUsers.getBGCountsIn(mIds)]);
+    logger.info('occasions ', JSON.stringify(occasions));
+    logger.info('bgcounts ', JSON.stringify(bgcounts));
+    for (let i = 0; i < occasions.count; i += 1) {
+      const occasion = occasions.items[i];
+      [occasion.ouser] = mJoins.items.filter((item) => item.occasionId === occasion.id);
+      if (occasion.extras.brideId && !gbIds.includes(occasion.extras.brideId)) gbIds.push(occasion.extras.brideId);
+      if (occasion.extras.groomId && !gbIds.includes(occasion.extras.groomId)) gbIds.push(occasion.extras.groomId);
+      const bg = bgcounts.items.filter((item) => item.occasionId === occasion.id)[0];
+      occasion.groomCount = bg.groomCount;
+      occasion.brideCount = bg.brideCount;
+      // no side count is added, referece:occasion_user_js
+      occasion.noSideCount = bg.noSideCount;
+      occasions.items[i] = occasion;
+    }
+    logger.info('groom & bride ids ', JSON.stringify(gbIds));
+    const gbUsers = await rdsUsers.getUserFieldsIn(gbIds, [...constants.MINI_PROFILE_FIELDS, 'facebook', 'instagram', 'createdAt', 'updatedAt']);
+    logger.info('gbUsers ', JSON.stringify(gbUsers));
+    for (let i = 0; i < occasions.count; i += 1) {
+      if (occasions.items[i].extras.brideId) [occasions.items[i].extras.bride] = gbUsers.items.filter((item) => item.id === occasions.items[i].extras.brideId);
+      if (occasions.items[i].extras.groomId) [occasions.items[i].extras.groom] = gbUsers.items.filter((item) => item.id === occasions.items[i].extras.groomId);
+    }
+    return occasions;
+  }
+
+  // if (type === 'events') return rdsMEvents.getEventsIn(mIds);
+  // if (type === 'assets') return rdsAssets.getParentAssetsIn(mIds.map((i) => `occasion_${i}`));
+  if (type === 'users') {
+    const oUsers = await rdsOUsers.getOUsersIn(vIds);
+    const userIds = oUsers.items.map((user) => user.userId);
+    const miniProfiles = await rdsUsers.getUserFieldsIn(userIds, constants.MINI_PROFILE_FIELDS);
+    for (let i = 0; i < oUsers.count; i += 1) {
+      [oUsers.items[i].user] = miniProfiles.items.filter((item) => item.id === oUsers.items[i].userId);
+    }
+    return oUsers;
+  }
+  return errors.handleError(400, 'invalid request type');
+}
 
 
 async function createNewOccasion(request) {
@@ -39,9 +89,9 @@ async function createNewOccasion(request) {
   ]);
 
   // const postId = (await rdsPosts.insertPost({ userId: decoded.id, occationId: insertId, type: 'occasion.join', status: 'A' })).insertId;
-  // await snsHelper.pushToSNS('timeline-bg-tasks', { service: 'timeline', component: 'post', action: 'add', data: { userId: decoded.id, weddingId: insertId, postId } });
+  // await snsHelper.pushToSNS('timeline-bg-tasks', { service: 'timeline', component: 'post', action: 'add', data: { userId: decoded.id, occasionId: insertId, postId } });
 
-  // TODO send an alert to indicate new wedding event was created
+  // TODO send an alert to indicate new occasion event was created
   request.pathParameters = { occasionId: insertId };
 
   return rdsOccasions.getOccasion(insertId);
@@ -55,9 +105,9 @@ async function invoke(event, context, callback) {
 
     let resp = {};
     switch (request.resourcePath) {
-      // case '/v1/list':
-      //   resp = await getOccasion(request);
-      //   break;
+      case '/v1/list':
+        resp = await getOccasions(request);
+        break;
 
       case '/v1/new':
         resp = await createNewOccasion(request);
