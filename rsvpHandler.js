665339c4a1f5c8b2c6729b245940e5e6a0030980
dhanushgowdahm <dhanushhm1012@gmail.com>
2025-05-26 12:12:27 +0530

    added rsvp apis

---
+++ rsvpHandler.js
@@ -0,0 +1,126 @@
+const _ = require('underscore');
+const logger = require('./bk-utils/logger');
+const access = require('./bk-utils/access');
+const errors = require('./bk-utils/errors');
+const constants = require('./bk-utils/constants');
+const rdsUsers = require('./bk-utils/rds/rds.users.helper');
+const rdsOccasions = require('./bk-utils/rds/rds.occasions.helper');
+const rdsRsvps = require('./bk-utils/rds/rds.occasion.rsvps.helper');
+const jwtHelper = require('./bk-utils/jwt.helper');
+
+
+async function getRsvpList(request) {
+  const { occasionId } = request.pathParameters;
+  const rsvp = await rdsRsvps.getRsvpList(`occasion_${occasionId}`);
+  return rsvp;
+}
+
+
+async function newOccasionRsvp(request) {
+  const { Authorization } = request.headers;
+  if (Authorization) request.decoded = jwtHelper.verifyToken(Authorization, process.env.jwtSecretKey);
+  const { occasionId } = request.pathParameters;
+
+  const obj = _.pick(request.body, ['rsvp', 'name', 'side', 'guests', 'accomodation']);
+  if (request.decoded) {
+    obj.userId = request.decoded.id;
+    obj.name = request.decoded.username;
+  }
+  const occasion = await rdsOccasions.getOccasion(occasionId);
+  if (_.isEmpty(occasion)) errors.handleError(404, 'occasion not found');
+  if (occasion.isPublic === false) errors.handleError(401, 'requested occasion is private');
+  logger.info('rsvp occasion request for ', occasionId);
+  await rdsRsvps.insertNewRsvp({ parentId: `occasion_${occasionId}`, obj });
+  request.pathParameters.occasionId = occasionId;
+  return getRsvpList(request);
+}
+
+
+async function getRsvpByUser(request) {
+  const { decoded } = request.decoded;
+  const { occasionId, userId } = request.pathParameters;
+  if (decoded.id !== userId) errors.handleError(401, 'unauthorized');
+
+  const rsvp = await rdsRsvps.getRsvpList(`occasion_${occasionId}`);
+  const uRsvp = _.find(rsvp.items, (item) => item.userId === userId);
+  if (!_.uRsvp) errors.handleError(404, 'rsvp not found for user');
+  return uRsvp;
+}
+
+
+async function updateRsvp(request) {
+  const { decoded, pathParameters, body } = request;
+  const { occasionId, userId } = pathParameters;
+  if (decoded.id !== userId) errors.handleError(401, 'unauthorized');
+  const obj = _.pick(body, ['rsvp', 'name', 'side', 'guests', 'accomodation']);
+  await rdsRsvps.updateRsvp({ parentId: `occasion_${occasionId}`, userId, obj });
+  request.pathParameters.occasionId = occasionId;
+  return getRsvpByUser(request);
+}
+
+
+async function getRsvpSummary(request) {
+  const { occasionId } = request.pathParameters;
+  const rsvp = await rdsRsvps.getRsvpList(`occasion_${occasionId}`);
+  const resp = { entity: 'rsvp', items: [], count: 0 };
+  if (!_.isEmpty(rsvp.items)) {
+    resp.items.users = await Promise.all(rsvp.items.map(async (item) => {
+      if (item.userId) {
+        const user = await rdsUsers.getUserFields(item.userId, constants.MINI_PROFILE_FIELDS);
+        return { ...item, user };
+      }
+      return item;
+    }));
+    const yUsers = _.filter(resp.items.users.rsvp, (user) => user.rsvp === 'Y');
+    resp.items.recents = _.first(yUsers, 5);
+    resp.items.count = yUsers.length;
+  }
+}
+
+async function invoke(event, context, callback) {
+  const headers = { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true };
+  try {
+    const request = access.validateRequest(event, context);
+
+    let resp = {};
+    switch (request.resourcePath) {
+      case '/v1/{occasionId}/rsvp':
+        resp = await newOccasionRsvp(request);
+        break;
+
+      case '/v1/{occasionId}/rsvp/{userId}':
+        switch (request.httpMethod) {
+          case 'GET': resp = await getRsvpByUser(request);
+            break;
+          case 'PUT': resp = await updateRsvp(request);
+            break;
+          default: errors.handleError(400, 'invalid request path');
+        }
+        break;
+
+      case '/v1/{occasionId}/rsvp/list':
+        resp = await getRsvpList(request);
+        break;
+
+      case '/v1/{occasionId}/rsvp/summary':
+        resp = await getRsvpSummary(request);
+        break;
+
+      default: errors.handleError(400, 'invalid request path');
+    }
+
+    context.callbackWaitsForEmptyEventLoop = false;
+    logger.info('final response ', JSON.stringify(resp));
+    return callback(null, { statusCode: 200, headers, body: JSON.stringify(resp) });
+  } catch (err) {
+    context.callbackWaitsForEmptyEventLoop = false;
+    logger.error('error processing api');
+    logger.error(err);
+    return callback(null, { headers, ...err });
+  }
+}
+
+
+module.exports = {
+  invoke,
+};
