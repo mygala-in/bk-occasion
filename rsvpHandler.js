26880a10b263d67d2b035290a8fd754fbddcf3ba
dhanushgowdahm <dhanushhm1012@gmail.com>
2025-05-27 09:30:59 +0530

    deploy

--- rsvpHandler.js
+++ rsvpHandler.js
@@ -56,7 +56,7 @@ async function getRsvpByUser(request) {
   if (decoded.id !== parseInt(userId, 10)) errors.handleError(401, 'unauthorized');
 
   const rsvp = await rdsRsvps.getRsvpList(`occasion_${occasionId}`);
-  const uRsvp = _.find(rsvp.items, (item) => item.userId === userId);
+  const uRsvp = _.find(rsvp.items, (item) => item.userId === parseInt(userId, 10));
   if (!_.uRsvp) errors.handleError(404, 'rsvp not found for user');
   return uRsvp;
 }
