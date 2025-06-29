b71a9f50382e47abaae9f852936e45a73db551ae
dhanushgowdahm <dhanushhm1012@gmail.com>
2025-06-29 13:24:27 +0530

    minor fix

--- handler.js
+++ handler.js
@@ -156,6 +156,7 @@ async function createNewOccasion(request) {
   await snsHelper.pushToSNS('post-bg-tasks', { service: 'post', component: 'post', action: 'add', data: { userId: decoded.id, parentId: `occasion_${insertId}`, type: 'join', status: 'A' } });
   const user = await rdsUsers.getUserFields(decoded.id, [...constants.MINI_PROFILE_FIELDS, 'phone', 'role']);
   const message = { content: `**New Invite**:\nLink: https://link.mygala.in/${code}/invite\nCreator: \n${JSON.stringify(user, null, 2)}` };
+  logger.info('pushing to discord-sns-notifier', message);
   await snsHelper.pushToSNS('discord-sns-notifier', { service: 'discord', channel: 'invite', action: 'notify', data: message });
   request.pathParameters = { occasionId: insertId };
   return getOccasion(request);
